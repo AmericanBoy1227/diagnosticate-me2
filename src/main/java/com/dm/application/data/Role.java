12bae144ec43a6bbd34d410347ea6c3ff3f42194
start.vaadin.com <no-reply@vaadin.com>
2023-02-14 15:24:44 +0000

    Generated project

---
+++ src/main/java/com/dm/application/data/Role.java
@@ -0,0 +1,5 @@
+package com.dm.application.data;
+
+public enum Role {
+    USER, ADMIN;
+}
---
+++ src/main/java/com/dm/application/data/service/SamplePersonRepository.java
@@ -0,0 +1,12 @@
+package com.dm.application.data.service;
+
+import com.dm.application.data.entity.SamplePerson;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface SamplePersonRepository
+        extends
+            JpaRepository<SamplePerson, Long>,
+            JpaSpecificationExecutor<SamplePerson> {
+
+}
